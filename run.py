abc8c3a6f6c490ec496274e6e91db07d80bf2022
ejasquith <emily.asquith@icloud.com>
2022-07-19 22:33:51 +0000

    Implement menu loop

--- run.py
+++ run.py
@@ -12,11 +12,36 @@ LOGO = """
 
 WELCOME_MESSAGE = "Welcome to VGCM - Video Game Collection Manager"
 
+MENU = """
+1) Add a game to your collection
+2) Display all games in your collection
+3) Search for a game in your collection
+4) Remove a game from your collection
+5) Exit
+
+> """
+
 
 def main():
     print(LOGO)
     print(WELCOME_MESSAGE)
-    print(tabulate(database.get_all_games()))
+                            
+    while (user_input := input(MENU)) != "5":
+        if user_input == "1":
+            # Add game
+            pass
+        elif user_input == "2":
+            # Display games
+            pass
+        elif user_input == "3":
+            # Search games
+            pass
+        elif user_input == "4":
+            # Remove game
+            pass
+        else:
+            # Invalid input
+            pass
 
 
 main()
