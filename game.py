87d54e9d30e674c35e502f94b21fe656a27a12c6
ejasquith <emily.asquith@icloud.com>
2022-07-19 00:14:45 +0000

    Define game class

---
+++ game.py
@@ -0,0 +1,24 @@
+"""
+Create game objects to represent items in a user's collection.
+Classes:
+    Game
+"""
+
+
+class Game:
+    """
+    A class to represent a game.
+    ----------
+    Attributes
+    ----------
+    title : string
+        title of the game
+    publisher : string
+        publisher of the game
+    platform : string
+        platform the game was released to
+    """
+    def __init__(self, title, publisher, platform):
+        self.title = title
+        self.publisher = publisher
+        self.platform = platform
