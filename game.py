c09f229fa6069f24a5ab04a918313c5ab9dbf36c
ejasquith <emily.asquith@icloud.com>
2022-07-19 20:18:43 +0000

    Add new properties to game class

--- game.py
+++ game.py
@@ -14,12 +14,25 @@ class Game:
     ----------
     title : string
         title of the game
+    genre : string
+        genre of the game
     publisher : string
         publisher of the game
+    developer : string
+        developer of the game
     platform : string
         platform the game was released to
+    release_date : datetime
+        Date of the game's release
+    purchase_date : datetime
+        Date the game was purchased        
     """
-    def __init__(self, title, publisher, platform):
+    def __init__(self, title, genre, publisher, developer,
+                 platform, release_date, purchase_date):
         self.title = title
+        self.genre = genre
         self.publisher = publisher
+        self.developer = developer
         self.platform = platform
+        self.release_date = release_date
+        self.purchase_date = purchase_date
