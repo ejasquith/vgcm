0f69d3be2cac9d3e38aaa1dee38217db7e9b0930
ejasquith <emily.asquith@icloud.com>
2022-07-19 00:05:30 +0000

    Print logo and welcome message on app startup

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+LOGO = """
+ _    ________________  ___
+| |  / / ____/ ____/  |/  /
+| | / / / __/ /   / /|_/ / 
+| |/ / /_/ / /___/ /  / /  
+|___/\____/\____/_/  /_/   
+                           
+"""
+
+WELCOME_MESSAGE = "Welcome to VGCM - Video Game Collection Manager"
+
+
+def main():
+    print(LOGO)
+    print(WELCOME_MESSAGE)
+
+
+main()
