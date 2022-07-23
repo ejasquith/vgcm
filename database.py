93d631b502483c0269dc1e0dabd54807cdd1c7dd
ejasquith <emily.asquith@icloud.com>
2022-07-23 13:02:07 +0000

    Refactor database.py to use class structure

--- database.py
+++ database.py
@@ -1,70 +1,82 @@
 """
-Handle connection to PostrgreSQL database.
+Create database object to handle operations
 
-Methods:
-    create_game
-    delete_game
-    find_game
-    get_all_games
+Classes:
+    Database
 """
 
 from datetime import datetime
 from game import Game
 
-# Connect to DB
-# Using Python data structures during development
 
-_records = [
-    Game("Fallout: New Vegas", "RPG", "Bethesda Softworks",
-         "Obsidian Entertainment", "Xbox 360",
-         datetime(2010, 10, 22), datetime(2022, 6, 3)),
-    Game("The Outer Worlds", "RPG", "Private Division",
-         "Obsidian Entertainment", "PC",
-         datetime(2019, 10, 25), datetime(2022, 7, 17)),
-    Game("Sonic the Hedgehog", "Platformer", "Sega",
-         "Sonic Team", "Sega Genesis",
-         datetime(1991, 6, 23), datetime(2012, 4, 18))
-]
-
-
-def create_game(title, genre, publisher, developer,
-                platform, release_date, purchase_date):
+class Database:
     """
-    Creates game object with given parameters and inserts into database
-    """
-    _records.append(
-        Game(title, genre, publisher, developer,
-             platform, release_date, purchase_date)
-    )
+    A class used to handle database operations.
 
-
-def delete_game(title, genre, publisher, developer,
-                platform, release_date, purchase_date):
+    Attributes:
+        _records : list
+    Methods:
+        create_game
+        delete_game
+        find_game
+        get_all_games
+        load_file
+        save_file
     """
-    Finds and deletes game with given parameters.
-    If no game exists, returns None
-    """
-    pass
 
+    def __init__(self, file):
+        if file is not None:
+            # Parse file and load
+            self.load_file(file)
+        else:
+            self._records = []
 
-def find_game(**kwargs):
-    """
-    Returns game objects that match given parameters.
-    Parameters are optional but at least one must be given
+    def create_game(self, title, genre, publisher, developer,
+                    platform, release_date, purchase_date):
+        """
+        Creates game object with given parameters and inserts into database
+        """
+        self._records.append(
+            Game(title, genre, publisher, developer,
+                 platform, release_date, purchase_date)
+        )
 
-    Accepted parameters:
-        title : str
-        genre : str
-        publisher : str
-        developer : str
-        platform : str
-    """
-    # Use SQL where statements to easily filter
-    pass
+    def delete_game(self, title, genre, publisher, developer,
+                    platform, release_date, purchase_date):
+        """
+        Finds and deletes game with given parameters.
+        If no game exists, returns None
+        """
+        pass
 
+    def find_game(self, **kwargs):
+        """
+        Returns game objects that match given parameters.
+        Parameters are optional but at least one must be given
 
-def get_all_games():
-    """
-    Returns a list of all game objects in database.
-    """
-    return _records
+        Accepted parameters:
+            title : str
+            genre : str
+            publisher : str
+            developer : str
+            platform : str
+        """
+        pass
+
+    def get_all_games(self):
+        """
+        Returns a list of all game objects in database.
+        """
+        return self._records
+
+    def load_file(self, file):
+        """
+        Loads _records attribute from csv file
+        """
+        pass
+
+    def save_file(self, file_name):
+        """
+        Saves records to csv file
+        """
+        pass
