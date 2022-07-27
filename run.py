57ec02dfe3c1f6e796fd80ca958cefe4befa85e8
ejasquith <emily.asquith@icloud.com>
2022-07-27 17:56:48 +0000

    Add params to prompt methods to allow empty strings

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def format_table_output(games):
                              "Platform", "Release Date", "Purchase Date"])
 
 
-def prompt_string_input(prompt):
+def prompt_string_input(prompt, allow_empty):
     """
     Prompts user to enter a non-empty string
     Returns valid input
@@ -42,12 +42,14 @@ def prompt_string_input(prompt):
         user_input = input(prompt)
         if user_input:
             valid = True
+        elif allow_empty:
+            valid = True
         else:
             print("String must not be empty. Please try again.")
     return user_input
 
 
-def prompt_date_input(prompt):
+def prompt_date_input(prompt, allow_empty):
     """
     Prompts user to enter a date
     Returns datetime object from valid input
@@ -57,14 +59,17 @@ def prompt_date_input(prompt):
         try:
             date = datetime.strptime(input(prompt), "%d/%m/%Y")
         except ValueError:
-            print("Date in invalid format. Should be in format DD/MM/YYYY")
-            print("Please try again.")
+            if allow_empty:
+                valid = True
+            else:
+                print("Date in invalid format. Should be in format DD/MM/YYYY")
+                print("Please try again.")
         else:
             valid = True
     return date
 
 
-def prompt_game_details_input():
+def prompt_game_details_input(allow_empty):
     """
     Prompts user to enter details for new Game object
     Returns list of inputted values
@@ -72,13 +77,13 @@ def prompt_game_details_input():
     values = []
 
     print("\nEnter game details:")
-    values.append(prompt_string_input("Title: "))
-    values.append(prompt_string_input("Genre: "))
-    values.append(prompt_string_input("Publisher: "))
-    values.append(prompt_string_input("Developer: "))
-    values.append(prompt_string_input("Platform: "))
-    values.append(prompt_date_input("Release date: "))
-    values.append(prompt_date_input("Purchase date: "))
+    values.append(prompt_string_input("Title: ", allow_empty))
+    values.append(prompt_string_input("Genre: ", allow_empty))
+    values.append(prompt_string_input("Publisher: ", allow_empty))
+    values.append(prompt_string_input("Developer: ", allow_empty))
+    values.append(prompt_string_input("Platform: ", allow_empty))
+    values.append(prompt_date_input("Release date: ", allow_empty))
+    values.append(prompt_date_input("Purchase date: ", allow_empty))
 
     return values
 
@@ -95,7 +100,7 @@ def main():
     while (user_input := input(MENU)) != "5":
         if user_input == "1":
             # Create new game
-            values = prompt_game_details_input()
+            values = prompt_game_details_input(False)
             database.create_game(values)
         elif user_input == "2":
             # Display games
