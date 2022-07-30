0791b0a5fc889fceceae9aea0b26bfc589b786b3
ejasquith <emily.asquith@icloud.com>
2022-07-30 03:10:11 +0000

    Break up long lines of code

--- database.py
+++ database.py
@@ -63,7 +63,8 @@ class Database:
         """
         searched_list = copy(self._records)
         for key, value in kwargs.items():
-            searched_list = [record for record in searched_list if getattr(record, key) == value]
+            searched_list = [record for record in searched_list
+                             if getattr(record, key) == value]
         return searched_list
 
     def get_all_games(self):
--- run.py
+++ run.py
@@ -113,12 +113,13 @@ def main():
             print("\n"+format_table_output(games))
         elif user_input == "4":
             # Remove game
-            print("Enter details of game(s) to delete, or leave blank to skip field")
+            print("Enter details of game(s) to delete,\
+                  or leave blank to skip field")
             values = prompt_game_details_input(True)
             # Remove entries with empty values
             values = {key: value for key, value in values.items() if value}
             games = database.find_game(**values)
-            
+
             if games:
                 print("\n"+format_table_output(games))
                 print("Are you sure you want to delete these games? (y/n)")
