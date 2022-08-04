01d7bb8b0c1869810abc517acba30ca3beddf012
ejasquith <emily.asquith@icloud.com>
2022-08-04 15:09:51 +0000

    Implement SheetConnection class

--- sheetconnection.py
+++ sheetconnection.py
@@ -1,15 +1,33 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-]
+class SheetConnection():
+    """
+    Class used to handle connection to Google Sheet API
+    Should never be instantiated directly; use SheetConnection.get_instance()
+    """
+    instance = None
 
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    def __init__(self):
+        SheetConnection.instance = self
 
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+        self._SCOPE = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive"
+        ]
 
-SHEET = GSPREAD_CLIENT.open("vgcm_data")
+        self._CREDS = Credentials.from_service_account_file("creds.json")
+        self._SCOPED_CREDS = self._CREDS.with_scopes(self._SCOPE)
+
+        self._GSPREAD_CLIENT = gspread.authorize(self._SCOPED_CREDS)
+
+        self._SHEET = self._GSPREAD_CLIENT.open("vgcm_data")
+
+    def get_instance(self):
+        """
+        Returns a single instance of SheetConnection
+        """
+        if SheetConnection.instance is None:
+            SheetConnection.instance = SheetConnection()
+        return SheetConnection.instance
