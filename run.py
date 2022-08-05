54967bc6590cc598a32bda77f55d4dd7d06856a2
ejasquith <emily.asquith@icloud.com>
2022-08-05 18:03:42 +0000

    Edit max col widths to allow for piping between cols

--- run.py
+++ run.py
@@ -41,7 +41,7 @@ def format_table_output(games):
     return tabulate(
         games_properties,
         headers=["Title", "Genre", "Publisher", "Platform", "Release Date"],
-        maxcolwidths=[16, 16, 16, 16, 16]
+        maxcolwidths=[15, 15, 15, 15, 15]
     )
 
 
