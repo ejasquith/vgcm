0a6f1f42a23847d108c4942bd25bcbd73340d972
ejasquith <emily.asquith@icloud.com>
2022-08-06 18:09:53 +0000

    Add docstring to sheetconnection.py

--- sheetconnection.py
+++ sheetconnection.py
@@ -1,3 +1,10 @@
+"""
+Creates SheetConnection object to handle connection
+to Google Sheets using gspread
+Classes:
+    SheetConnection
+"""
+
 import gspread
 from google.oauth2.service_account import Credentials
 
